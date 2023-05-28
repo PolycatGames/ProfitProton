4edbcc68716f9d2d89202deb160c89868b0b183c
Polycat Games <polycatgames.business@gmail.com>
2023-05-28 19:59:22 +0200

    About Test

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!--Meta Data-->
+  <title>ENTER TITLE</title>
+  <meta name="description" content="ENTER DESCRIPTION" />
+  <meta name="keywords" content="KEYWORDS" />
+  <meta name="author" content="AUTHOR" />
+  <!--<meta name="robots" content="index, follow">-->
+
+  <!--Styles-->
+  <link rel="stylesheet" href="styles/about.css">
+
+  <?php include 'assets/head.html'; ?>
+</head>
+
+<body>
+  <?php include 'assets/headernav.html'; ?>
+
+  <main class="main-content">
+    <article class="content-holder">
+      <section>
+        Welcome to ProfitProton
+      </section>
+      <section class="about-text">
+        <p>At ProfitProton we are dedicated to providing you with valuable insights and practical strategies to help you unlock the potential of making money online.</p>
+
+        <p>Whether you're looking for ways to generate a passive income, explore freelance opportunities, or start your own online business, we've got you covered. Our articles are carefully crafted to provide step-by-step guidance, proven techniques, and real-life examples to empower you on your journey towards financial freedom.</p>
+
+        <p>Here, we believe that everyone has the ability to harness the power of the internet and create a sustainable income stream. We understand that the online world can sometimes feel overwhelming with its vast possibilities, but our goal is to simplify the process and help you navigate through the noise.</p>
+
+        <p>By joining our community, you'll gain access to a wealth of information, resources, and a supportive network of like-minded individuals. We encourage active participation, so feel free to engage in discussions, share your success stories, and ask questions. Together, we can learn and grow, continuously adapting to the ever-evolving digital landscape.</p>
+
+        <p>We are committed to transparency and integrity, which is why we only recommend tried-and-tested methods that have proven results. Our aim is to equip you with the tools and knowledge necessary to make informed decisions and take confident steps towards achieving your financial goals.</p>
+
+        <p>Have any questions? Feel free to <a href="/contact.php">reach out to us!</a></p>
+
+      </section>
+    </article>
+  </main>
+
+  <?php include 'assets/footer.html'; ?>
+</body>
+
+</html>
