9e818079a2ab0c766d0363ae9131454e93ad7c0e
Polycat Games <polycatgames.business@gmail.com>
2023-06-02 10:12:09 +0200

    Final Fix

--- assets/head.php
+++ assets/head.php
@@ -91,3 +91,48 @@
       .catch(error => console.log(error));
   });
 </script>
+
+<script>
+  let burgerActivated = false;
+  let sidebar;
+
+  function setSidebar() {
+    console.log("Ran?");
+    sidebar = document.querySelector(".sidebar");
+    if (sidebar !== null) {
+      sidebar.style.display = "none";
+      console.log(sidebar + " found");
+    } else {
+      console.log(sidebar + " not found");
+    }
+  }
+
+  function activateBurger() {
+    if (burgerActivated == false) {
+      sidebar.style.display = "flex";
+      burgerActivated = true;
+      console.log("inactive -> active")
+    } else {
+      deactivateBurger();
+      console.log("active -> inactive")
+    }
+  }
+
+  function deactivateBurger() {
+    sidebar.style.display = "none";
+    burgerActivated = false;
+  }
+
+
+
+  window.addEventListener('resize', () => {
+    // Get the width of the window
+    const windowWidth = window.innerWidth;
+
+    // Do something with the width
+    if (windowWidth > 760) {
+      sidebar.style.display = "none";
+      burgerActivated = false;
+    }
+  });
+</script>
