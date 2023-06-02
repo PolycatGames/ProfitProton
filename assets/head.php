8e6f57b8c6f933cbdfb6fcd0e28cc69c6300fc65
Polycat Games <polycatgames.business@gmail.com>
2023-06-02 10:41:53 +0200

    Fix

--- assets/head.php
+++ assets/head.php
@@ -71,26 +71,62 @@
 </script>
 
 <script>
-  document.addEventListener('DOMContentLoaded', function() {
+  function loadUpdatedStylesheet() {
     fetch('https://api.github.com/repos/PolycatGames/ProfitProton/commits')
       .then(response => response.json())
       .then(data => {
         var latestCommit = data[0].sha; // Extract the latest commit hash
-        var links = document.querySelectorAll('link[rel="stylesheet"], script[src]');
+        var links = document.querySelectorAll('link[rel="stylesheet"]');
+        var preloadPromises = [];
+
         for (var i = 0; i < links.length; i++) {
           var link = links[i];
           var href = link.getAttribute('href');
-          var src = link.getAttribute('src');
+
           if (href) {
-            link.setAttribute('href', href + '?v=' + latestCommit);
-          } else if (src) {
-            link.setAttribute('src', src + '?v=' + latestCommit);
+            var updatedHref = href + '?v=' + latestCommit;
+            var preloadPromise = new Promise((resolve, reject) => {
+              var preloadLink = document.createElement('link');
+              preloadLink.setAttribute('rel', 'preload');
+              preloadLink.setAttribute('as', 'style');
+              preloadLink.setAttribute('href', updatedHref);
+              preloadLink.addEventListener('load', resolve);
+              preloadLink.addEventListener('error', reject);
+              document.head.appendChild(preloadLink);
+            });
+
+            preloadPromises.push(preloadPromise);
           }
         }
+
+        Promise.all(preloadPromises)
+          .then(() => {
+            for (var i = 0; i < links.length; i++) {
+              var link = links[i];
+              var href = link.getAttribute('href');
+
+              if (href) {
+                var updatedHref = href + '?v=' + latestCommit;
+                var newLink = document.createElement('link');
+                newLink.setAttribute('rel', 'stylesheet');
+                newLink.setAttribute('href', updatedHref);
+                document.head.appendChild(newLink);
+                link.parentNode.removeChild(link);
+              }
+            }
+
+            // All stylesheets have been loaded, remove the default stylesheet
+            var defaultStylesheet = document.querySelector('link[href="default.css"]');
+            defaultStylesheet.parentNode.removeChild(defaultStylesheet);
+          })
+          .catch(error => console.log(error));
       })
       .catch(error => console.log(error));
-  });
+  }
+
+  loadUpdatedStylesheet();
 </script>
+
 
 <script>
   let burgerActivated = false;
