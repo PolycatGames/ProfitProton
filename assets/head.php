c384e1ea1689ccacaa01d45049977522bf46a226
Polycat Games <polycatgames.business@gmail.com>
2023-06-02 22:29:03 +0200

    Test

--- assets/head.php
+++ assets/head.php
@@ -71,81 +71,78 @@
 </script>
 
 <script>
-  document.addEventListener("DOMContentLoaded", function() {
-    function loadUpdatedStylesheet() {
-      if (location.hostname !== "localhost") {
-        // Create a blank screen overlay
-        var overlay = document.createElement('div');
-        overlay.style.position = 'fixed';
-        overlay.style.top = '0';
-        overlay.style.left = '0';
-        overlay.style.width = '100vw';
-        overlay.style.height = '100vh';
-        overlay.style.background = '#fff'; // Set the background color to match your website's background
+  function loadUpdatedStylesheet() {
+    if (location.hostname === "localhost") {
+      // Create a blank screen overlay
+      var overlay = document.createElement('div');
+      overlay.style.position = 'fixed';
+      overlay.style.top = '0';
+      overlay.style.left = '0';
+      overlay.style.width = '100vw';
+      overlay.style.height = '100vh';
+      overlay.style.background = '#fff'; // Set the background color to match your website's background
 
-        // Add the overlay to the body
-        document.body.appendChild(overlay);
+      // Add the overlay to the body
+      document.body.appendChild(overlay);
 
-        fetch('https://api.github.com/repos/PolycatGames/ProfitProton/commits')
-          .then(response => response.json())
-          .then(data => {
-            var latestCommit = data[0].sha; // Extract the latest commit hash
-            var links = document.querySelectorAll('link[rel="stylesheet"]');
-            var preloadPromises = [];
+      fetch('https://api.github.com/repos/PolycatGames/ProfitProton/commits')
+        .then(response => response.json())
+        .then(data => {
+          var latestCommit = data[0].sha; // Extract the latest commit hash
+          var links = document.querySelectorAll('link[rel="stylesheet"]');
+          var preloadPromises = [];
 
-            for (var i = 0; i < links.length; i++) {
-              var link = links[i];
-              var href = link.getAttribute('href');
+          for (var i = 0; i < links.length; i++) {
+            var link = links[i];
+            var href = link.getAttribute('href');
 
-              if (href) {
-                var updatedHref = href + '?v=' + latestCommit;
-                var preloadPromise = new Promise((resolve, reject) => {
-                  var preloadLink = document.createElement('link');
-                  preloadLink.setAttribute('rel', 'preload');
-                  preloadLink.setAttribute('as', 'style');
-                  preloadLink.setAttribute('href', updatedHref);
-                  preloadLink.addEventListener('load', resolve);
-                  preloadLink.addEventListener('error', reject);
-                  document.head.appendChild(preloadLink);
-                });
+            if (href) {
+              var updatedHref = href + '?v=' + latestCommit;
+              var preloadPromise = new Promise((resolve, reject) => {
+                var preloadLink = document.createElement('link');
+                preloadLink.setAttribute('rel', 'preload');
+                preloadLink.setAttribute('as', 'style');
+                preloadLink.setAttribute('href', updatedHref);
+                preloadLink.addEventListener('load', resolve);
+                preloadLink.addEventListener('error', reject);
+                document.head.appendChild(preloadLink);
+              });
 
-                preloadPromises.push(preloadPromise);
-              }
+              preloadPromises.push(preloadPromise);
             }
+          }
 
-            Promise.all(preloadPromises)
-              .then(() => {
-                for (var i = 0; i < links.length; i++) {
-                  var link = links[i];
-                  var href = link.getAttribute('href');
+          Promise.all(preloadPromises)
+            .then(() => {
+              for (var i = 0; i < links.length; i++) {
+                var link = links[i];
+                var href = link.getAttribute('href');
 
-                  if (href) {
-                    var updatedHref = href + '?v=' + latestCommit;
-                    var newLink = document.createElement('link');
-                    newLink.setAttribute('rel', 'stylesheet');
-                    newLink.setAttribute('href', updatedHref);
-                    document.head.appendChild(newLink);
-                    link.parentNode.removeChild(link);
-                  }
+                if (href) {
+                  var updatedHref = href + '?v=' + latestCommit;
+                  var newLink = document.createElement('link');
+                  newLink.setAttribute('rel', 'stylesheet');
+                  newLink.setAttribute('href', updatedHref);
+                  document.head.appendChild(newLink);
+                  link.parentNode.removeChild(link);
                 }
+              }
 
-                // All stylesheets have been loaded, remove the default stylesheet
-                var defaultStylesheet = document.querySelector('link[href="default.css"]');
-                defaultStylesheet.parentNode.removeChild(defaultStylesheet);
+              // All stylesheets have been loaded, remove the default stylesheet
+              var defaultStylesheet = document.querySelector('link[href="default.css"]');
+              defaultStylesheet.parentNode.removeChild(defaultStylesheet);
 
-                // Remove the overlay to reveal the updated content
-                document.body.removeChild(overlay);
-              })
-              .catch(error => console.log(error));
-          })
-          .catch(error => console.log(error));
-      }
+              // Remove the overlay to reveal the updated content
+              document.body.removeChild(overlay);
+            })
+            .catch(error => console.log(error));
+        })
+        .catch(error => console.log(error));
     }
+  }
 
-    loadUpdatedStylesheet();
-  });
+  document.addEventListener("DOMContentLoaded", loadUpdatedStylesheet);
 </script>
-
 
 
 
