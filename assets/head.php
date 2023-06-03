47eb98bd7504b9459a4eeca08e07a43f5fbee5d9
Polycat Games <polycatgames.business@gmail.com>
2023-06-03 14:17:28 +0200

    Google Analytics

--- assets/head.php
+++ assets/head.php
@@ -3,30 +3,21 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 <!--HTML-->
 <link rel="import" href="assets/headernav.html" />
+
 <!--Fonts-->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
 <!--Standard Styles-->
 <link rel="stylesheet" href="styles/general.css">
 <link rel="stylesheet" href="styles/header.css">
 <link rel="stylesheet" href="styles/footer.css">
 <link rel="stylesheet" href="styles/sidebar.css">
-<!--Standard Elements-->
-<style>
-  .whitescreen {
-    position: fixed;
-    z-index: 9999;
-    background-color: rgb(255, 255, 255);
-    right: 0;
-    bottom: 0;
-    left: 0;
-    top: 0;
-  }
-</style>
-<div class="whitescreen"></div>
+
 <!--Standard Scripts-->
 <script>
   if (location.hostname !== "localhost !") {
@@ -247,4 +238,31 @@
       burgerActivated = false;
     }
   });
+</script>
+
+<!--Standard Elements-->
+<style>
+  .whitescreen {
+    position: fixed;
+    z-index: 9999;
+    background-color: rgb(255, 255, 255);
+    right: 0;
+    bottom: 0;
+    left: 0;
+    top: 0;
+  }
+</style>
+<div class="whitescreen"></div>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TZ5GYYLM28"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-TZ5GYYLM28');
 </script>
