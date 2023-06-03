0edca308c4081fd6eb89529cbd42f7e9bb1a5e15
Polycat Games <polycatgames.business@gmail.com>
2023-06-03 11:11:37 +0200

    Test

--- assets/head.php
+++ assets/head.php
@@ -15,7 +15,7 @@
 <link rel="stylesheet" href="styles/footer.css">
 <link rel="stylesheet" href="styles/sidebar.css">
 <!--Standard Elements-->
-<!--<div class="whitescreen"></div>-->
+<div class="whitescreen"></div>
 <!--Standard Scripts-->
 <script>
   if (location.hostname !== "localhost !") {
