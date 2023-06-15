c033e265d80bbd3483ae3b616eeb126b75be8e75
Polycat Games <polycatgames.business@gmail.com>
2023-06-15 11:50:25 +0200

    Subscribe Success

--- blog.php
+++ blog.php
@@ -389,9 +389,12 @@
                     <input class="subscribe-input" autocomplete="email" type="text" name="Email" placeholder="Enter your email" required>
                     <button class="subscribe-button" type="submit">Subscribe</button>
                 </form>
+                <div class="success" style="display: block;">Thanks for subscribing!</div>
                 <script>
                     const scriptURL = 'https://script.google.com/macros/s/AKfycbwxyoNzcKAiXGGTB-e9zgGu_v7atgUJXWpQEL7GImsFfWoDiFXOLeW07DmBCmt29ZS62w/exec'
                     const form = document.forms['submit-to-google-sheet']
+                    const inputElement = form.elements['Email']
+                    const successElement = document.querySelector('.success')
 
                     form.addEventListener('submit', e => {
                         e.preventDefault()
@@ -399,7 +402,11 @@
                                 method: 'POST',
                                 body: new FormData(form)
                             })
-                            .then(response => console.log('Success!', response))
+                            .then(response => {
+                                console.log('Success!', response)
+                                inputElement.value = '' // Reset the input value to an empty string
+                                successElement.style.visibility = 'visible' // Make the success visible
+                            })
                             .catch(error => console.error('Error!', error.message))
                     })
                 </script>
