9a8b0ca2314e4b1c264e5cbd164fb232b69c3db3
Polycat Games <polycatgames.business@gmail.com>
2023-07-06 18:20:01 +0200

    Test

--- assets/article/read-more.php
+++ assets/article/read-more.php
@@ -1,7 +1,7 @@
 <div class="articles-3x-small">
     <span class="articles-3x-small-title">Read More</span>
     <div class="articles-3x-small-grid">
-        <?
+        <?php
         // Read the contents of the text file
         $data = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/data.txt');
 
@@ -52,8 +52,8 @@
 
 
 
-        <? for ($i = 0; $i < $articleCount; $i++) : ?>
-            <? if ($showarticle[$i] !== 1) : ?>
+        <?php for ($i = 0; $i < $articleCount; $i++) : ?>
+            <?php if ($showarticle[$i] !== 1) : ?>
                 <a href="<?= $dataArray[$i]['link']; ?>" aria-label="Link to <?= $dataArray[$i]['title']; ?>">
                     <div class="article-3x-small">
                         <img src="<?= $dataArray[$i]['thumbnail'] ?>_1x.webp" alt="<?= $dataArray[$i]['title']; ?>" srcset="<?= $dataArray[$i]['thumbnail']?>_3x.webp 630w, <?= $dataArray[$i]['thumbnail']?>_1x.webp 1000w">
@@ -61,7 +61,7 @@
                         <span class="article-3x-small-date"><?= $dataArray[$i]['date']; ?></span>
                     </div>
                 </a>
-            <? endif; ?>
-        <? endfor; ?>
+            <?php endif; ?>
+        <?php endfor; ?>
     </div>
 </div>
