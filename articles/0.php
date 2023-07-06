bb4afd8789ace30e1881dd0b3a1ffc8819237433
Polycat Games <polycatgames.business@gmail.com>
2023-07-06 18:04:26 +0200

    test

--- articles/0.php
+++ articles/0.php
@@ -3,9 +3,9 @@
 
 <head>
     <!--Get Data-->
-    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/getdata.php'; ?>
-    <? define('num', 1); ?>
-    <? $data = getDataFromTextFile(num); ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/getdata.php'; ?>
+    <?php define('num', 1); ?>
+    <?php $data = getDataFromTextFile(num); ?>
 
     <!--Meta Data-->
     <title><?= $data['title'] ?></title>
@@ -14,32 +14,32 @@
     <meta name="author" content="<?= $data['author'] ?>" />
     <!--<meta name="robots" content="index, follow">-->
 
-    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/metadata.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/metadata.php'; ?>
 
     <!--Styles-->
-    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/head.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/head.php'; ?>
 </head>
 
 <body>
-    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/headernav.html'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/headernav.html'; ?>
 
     <main class="main-content">
         <article class="content-holder">
             <section class="pc">
                 <div class="pcl">
-                    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/pcl-front.php'; ?>
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/pcl-front.php'; ?>
 
                     Content Here
 
-                    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/read-more.php'; ?>
-                    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/subscribe.php'; ?>
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/read-more.php'; ?>
+                    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/subscribe.php'; ?>
                 </div>
-                <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/pcr.php'; ?>
+                <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/article/pcr.php'; ?>
             </section>
         </article>
     </main>
 
-    <? include $_SERVER['DOCUMENT_ROOT'] . '/assets/footer.html'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/assets/footer.html'; ?>
 </body>
 
 </html>
