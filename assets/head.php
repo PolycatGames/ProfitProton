e1bb16142f72518acaabea101ae826b47eab1033
Polycat Games <polycatgames.business@gmail.com>
2023-07-12 19:56:35 +0200

    Fixes

--- assets/head.php
+++ assets/head.php
@@ -1,5 +1,5 @@
 <!--Standard Meta Data-->
-<link rel="icon" type="image/x-icon" href="/images/logo/profitproton-favicon.png" />
+<link rel="icon" type="image/png" href="/images/logo/profitproton-favicon.png" />
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
