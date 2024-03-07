fb3c5e290028d24a0cb9112fcf9f2a9ed8dae86d
Polycat Games <polycatgames.business@gmail.com>
2024-03-07 09:16:10 +0100

    Fiverr Affiliate 2

--- assets/article/pcr.php
+++ assets/article/pcr.php
@@ -151,4 +151,16 @@
         <?php endfor; ?>
     </div>
 
+    <span class="pcr-disclosure pcr-holder">
+        Disclosure: This article may contain affiliate links, which means we may earn a small commission if a reader clicks through and makes a purchase.
+    </span>
+
+    <a class="pcr-promo" rel="noopener nofollow sponsored" target="_blank" href="/fiverr">
+        <div class="promo-banner">
+            <img id="pcr-promo-img" src="/images/fiverr-banner/fiverr-banner-small.webp" alt="Fiverr">
+            <svg class="promo-button-small" fill="white" xmlns="http://www.w3.org/2000/svg" id="arrow-circle-down" viewBox="0 0 24 24" width="512" height="512">
+                <path d="M0,12A12,12,0,1,0,12,0,12.013,12.013,0,0,0,0,12ZM14.535,6.293l3.586,3.586h0a3,3,0,0,1,0,4.243l-3.586,3.585-.025.024a1,1,0,1,1-1.389-1.438L16.414,13,6,13.007a1,1,0,1,1,0-2L16.413,11,13.121,7.707a1,1,0,1,1,1.414-1.414Z" />
+            </svg>
+        </div>
+    </a>
 </div>
